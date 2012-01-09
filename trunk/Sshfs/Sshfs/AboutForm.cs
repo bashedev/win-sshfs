56c6914df21eef943488a664cd7f752f416f7c2a
mladenovic.ml@gmail.com <mladenovic.ml@gmail.com@cff5ea8b-0d77-651b-d3f7-62d167a5fdf6>
2012-01-09 11:08:07 +0000

    0.0.1.3 source

---
+++ trunk/Sshfs/Sshfs/AboutForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using Renci.SshNet;
+
+namespace Sshfs
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutForm_Load(object sender, EventArgs e)
+        {
+            label1.Text = String.Format("Sshfs {0}", Assembly.GetEntryAssembly().GetName().Version);
+            label2.Text = String.Format("Dokan {0}.{1}.{2}.{3}",DokanNet.Dokan.Version / 1000, (DokanNet.Dokan.Version%1000) / 100, (DokanNet.Dokan.Version%100) / 10, DokanNet.Dokan.Version % 10);
+            label3.Text = String.Format("SSH.NET {0}", Assembly.GetAssembly(typeof (SshClient)).GetName().Version);
+
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(String.Format("http:\\{0}", (sender as LinkLabel).Text));
+        }
+    }
+}
